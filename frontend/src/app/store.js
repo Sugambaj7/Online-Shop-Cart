b94d02d27d0431861faed84361566cb4dcfc8ddc
Sugambaj7 <sugambaj123@gmail.com>
2024-08-04 11:12:55 +0545

    fetched data from backend using RTK query and displayed it in frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/productSlice";
 import { fetchProducts } from "../features/productSlice";
+import { productsApi } from "../features/productsAPI";
 
 const store = configureStore({
   reducer: {
     product: productReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 store.dispatch(fetchProducts());
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { useGetAllProductsQuery } from "../features/productsAPI";
 
 const Home = () => {
+  const { data, error, isLoading } = useGetAllProductsQuery();
   return (
     <div>
-      <h1 className="text-h1">Home</h1>
+      <div className="home-container">
+        {isLoading ? (
+          <p>Loading....</p>
+        ) : error ? (
+          <p>An error occurred...</p>
+        ) : (
+          <>
+            <h2 className="text-h2 font-semibold text-center mt-4">
+              New Arrivals
+            </h2>
+            <div className="product-section flex flex-wrap justify-between mt-8 ">
+              {data?.map((product) => (
+                <div
+                  className="product-card w-64 h-[55vh] max-w-[100%] flex flex-col justify-between my-4 mx-auto shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded p-4"
+                  key={product.id}
+                >
+                  <h3 className="text-2xl">{product.name}</h3>
+                  <img
+                    className="w-[90%] mt-3 ml-auto mr-auto"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                  <div className="details-section flex justify-between items-center">
+                    <span>{product.desc}</span>
+                    <span className="text-xl font-semibold">{product.price}</span>
+                  </div>
+                  <button className="bg-blue-500 py-1 px-3 text-white rounded w-[100%] h-9 flex justify-center items-center mt-8 font-semibold cursor-pointer tracking-wide">
+                    Add to Cart
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
